ef076f6c0288d817bfb85ab6269bcaef0597be0c
candle <mohammad.nl.chamaa@gmail.com>
2026-04-14 14:27:24 +0300

    install

--- public/script.js
+++ public/script.js
@@ -233,3 +233,10 @@ newChatBtn.onclick = createChat;
    INIT
 ======================= */
 createChat();
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/sw.js")
+      .then(reg => console.log("SW registered 🔥"))
+      .catch(err => console.log("SW failed:", err));
+  });
+}
